5ef9179d9d526d84e8fb11ec0f7aa2205e87d14d
tdstein <taylor.steinberg@posit.co>
2024-07-15 12:11:48 -0400

    --wip-- [skip ci]

---
+++ content/restart-content-using-airflow/dags/example.py
@@ -0,0 +1,25 @@
+import pendulum
+
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from posit import connect
+
+
+CONNECT_SERVER = Variable.get("CONNECT_SERVER")
+CONNECT_API_KEY = Variable.get("CONNECT_API_KEY")
+
+@dag(
+    dag_id="example",
+    schedule_interval="0 * * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False
+)
+def example():
+    @task
+    def restart():
+        client = connect.Client(CONNECT_SERVER, CONNECT_API_KEY)
+        content = client.content.get("5b6f05d1-1fea-480b-b8fa-51aec687a0bd")
+        task = content.deploy()
+        task.wait_for()
+    restart()
+example()
