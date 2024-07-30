5430fac9b8a5ce391e4d2665ff44fc793f6b604f
tdstein <taylor.steinberg@posit.co>
2024-07-30 13:26:46 -0400

    --wip-- [skip ci]

--- content/restarting-content-using-airflow/airflow/dags/simple_render.py
+++ content/restarting-content-using-airflow/airflow/dags/simple_render.py
@@ -1,25 +1,27 @@
-
 import pendulum
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from posit import connect
 
-CONNECT_SERVER = Variable.get("CONNECT_SERVER")
-CONNECT_API_KEY = Variable.get("CONNECT_API_KEY")
-
 CONTENT_GUID = "a35ae786-16da-43c6-93d8-0c709766dc46"
 
+
 @dag(
-    dag_id="simple",
+    dag_id="example-render",
     schedule_interval="0 * * * *",
     start_date=pendulum.now(),
     catchup=False,
 )
-def simple_dag():
+def example():
     @task
-    def restart():
-        client = connect.Client(CONNECT_SERVER, CONNECT_API_KEY)
+    def render():
+        client = connect.Client(
+            Variable.get("CONNECT_SERVER"), Variable.get("CONNECT_API_KEY")
+        )
         content = client.content.get(CONTENT_GUID)
-        content.restart()
-    restart()
-simple_dag()
+        content.render()
+
+    render()
+
+
+example()
